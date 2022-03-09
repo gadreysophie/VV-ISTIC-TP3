dddecaf0cb039ed411df8071e0cb8c775d0a61eb
Sophie Gadrey <gadreysophie@gmail.com>
2022-03-09 17:23:03 +0100

    try to implement BinaryHeap

--- code/tp3-heap/src/main/java/fr/istic/vv/BinaryHeap.java
+++ code/tp3-heap/src/main/java/fr/istic/vv/BinaryHeap.java
@@ -1,17 +1,175 @@
 package fr.istic.vv;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 
 class BinaryHeap<T> {
 
-    public BinaryHeap(Comparator<T> comparator) {  }
+    public Noeud root;
+    public Comparator<T> comparator;
 
-    public T pop() { return null; }
+    public BinaryHeap(Comparator<T> comparator) {
+        this.root = null;
+        this.comparator = comparator;
+    }
 
-    public T peek() { return null; }
+    public T pop() {
+        T out = this.root.value;
+        this.root = remove(this.root);
+        return out;
+    }
 
-    public void push(T element) {  }
+    public T peek() {
+        T out = this.root.value;
+        return out;
+    }
 
-    public int count() { return 0; }
+    public void push(T element) {
+        add(element);
+    }
+
+    public int count() {
+        return countNode();
+    }
+
+
+    /**
+    Classe Noeud pour représentation sous forme de graphe
+     */
+    class Noeud {
+        T value;
+        Noeud lhs;
+        Noeud rhs;
+
+        public Noeud (T element){
+        }
+
+        public Noeud(){
+        }
+    }
+
+    /**
+     * Pour retirer un noeud du graphe à la racine
+     * et redimensioner le graphe de façon à avoir un graphe trié
+     * @param root le noeud à enlever
+     * @return le noeud enlevé
+     */
+    private Noeud remove(Noeud root){
+        if (root.lhs != null & root.rhs != null){
+            if (0 > comparator.compare(root.lhs.value, root.rhs.value)){
+                Noeud out = root.lhs;
+                Noeud newrhs = root.rhs;
+                Noeud newlhs = remove(out);
+                out.lhs = newlhs;
+                out.rhs = newrhs;
+                return out;
+            } else {
+                Noeud out = root.rhs;
+                Noeud newlhs = root.lhs;
+                Noeud newrhs = remove(out);
+                out.rhs = newrhs;
+                out.lhs = newlhs;
+                return out;
+            }
+        }
+        if(root.lhs != null){
+            return root.lhs;
+        } else {
+            return root.rhs;
+        }
+    }
+
+
+
+    /**
+     * Pour faire le parcours en largeur du graphe et retrouver les valeurs des noeuds
+     * et ajouter l'élément de façon à avoir un graphe trié
+     * @param element élément à ajouter au graphe
+     */
+    private void add (T element) {
+        List<Noeud> noeuds = new ArrayList<>();
+        int index = 0;
+        noeuds.add(root);
+        while (true) {
+            Noeud n = noeuds.get(0);
+            if (n.lhs == null) {
+                n.lhs = new Noeud(element);
+                noeuds.add(n.lhs);
+                break;
+            } else if (n.rhs == null) {
+                n.rhs = new Noeud(element);
+                noeuds.add(n.rhs);
+                break;
+            } else {
+                noeuds.add(n.lhs);
+                noeuds.add(n.rhs);
+                index++;
+            }
+
+        }
+        index = noeuds.size() - 1;
+        Noeud n = noeuds.get(index);
+        Noeud parent = noeuds.get ((index-1)/2);
+        while(index > 0 && 0 > comparator.compare(n.value, parent.value)){
+
+            //invert
+            T temp = n.value;
+            n.value = parent.value ;
+            parent.value = temp;
+
+            //reverse index
+            noeuds.set((index-1)/2, n);
+            noeuds.set(index, parent);
+        }
+    }
+
+
+    private int countNode(){
+        List<Noeud> noeuds = new ArrayList<>();
+        int index = 0;
+        noeuds.add(root);
+        while (true) {
+            Noeud n = noeuds.get(0);
+            if (n.lhs == null) {
+                n.lhs = new Noeud();
+                noeuds.add(n.lhs);
+                break;
+            } else if (n.rhs == null) {
+                n.rhs = new Noeud();
+                noeuds.add(n.rhs);
+                break;
+            } else {
+                noeuds.add(n.lhs);
+                noeuds.add(n.rhs);
+                index++;
+            }
+
+        }
+        return index;
+    }
+
+/*
+    private void add2 (Noeud child) {
+        while((0 < comparator.compare(root.lhs.value, child.value)) &
+                (0 < comparator.compare(root.rhs.value, child.value)) ) {
+            if (root.lhs == null) {
+                root.lhs = child;
+                add(child);
+            } else if (root.rhs == null) {
+                root.rhs = child;
+                add(child);
+            } else {
+                if (0 < comparator.compare(root.lhs.value, root.rhs.value)) {
+                    Noeud rhs = root.rhs;
+                    rhs.rhs = child;
+                    add(child);
+                }
+            }
+        }
+    }
+
+ */
+
 
 }
