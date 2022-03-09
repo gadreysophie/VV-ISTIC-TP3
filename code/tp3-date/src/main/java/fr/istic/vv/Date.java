20af27a5e2a57b6c9d93694db8603cbd44a52cce
Sophie Gadrey <gadreysophie@gmail.com>
2022-03-09 09:27:28 +0100

    try to implement method of Date class

--- code/tp3-date/src/main/java/fr/istic/vv/Date.java
+++ code/tp3-date/src/main/java/fr/istic/vv/Date.java
@@ -1,17 +1,68 @@
 package fr.istic.vv;
 
+import java.util.Calendar;
+
+
 class Date implements Comparable<Date> {
 
-    public Date(int day, int month, int year) { }
+    Date date;
+    int dateDay;
+    int dateMonth;
+    int dateYear;
 
-    public static boolean isValidDate(int day, int month, int year) { return false; }
 
-    public static boolean isLeapYear(int year) { return false; }
+    public Date(int day, int month, int year) {
+        day = dateDay;
+        month = dateMonth;
+        year = dateYear;
+        date = new Date (day, month, year);
+    }
 
-    public Date nextDate() { return null; }
+    public int getDateDay(){
+        return dateDay;
+    }
+    public int getDateMonth(){
+        return dateMonth;
+    }
+    public int getDateYear(){
+        return dateYear;
+    }
 
-    public Date previousDate() { return null; }
+    public static boolean isValidDate(int day, int month, int year) {
 
-    public int compareTo(Date other) { return 0; }
+        if (day <= 0 || month <= 0 || year <= 0){
+            return false;}
+        if (month > 12 || day > 31){
+            return false;}
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, month - 1);
+
+        // Find the maximum field value possible for the day with the year and month.
+        int maxDay = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+        if (day > maxDay) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isLeapYear(int year) {
+        return year % 400 == 0 || ( year % 4 == 0 && year % 100 != 0 );
+    }
+
+    public Date nextDate() {
+        return date.nextDate();
+    }
+
+    public Date previousDate() {
+        return date.previousDate(); }
+
+    public int compareTo(Date other) {
+        if (other.equals(date)){
+        return 0;
+        }
+        else return -1;
+    }
 
 }
